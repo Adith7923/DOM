a839e8cd05dba8062623510cb46340b18ae47451
Adith7923 <adith7923@gmail.com>
2024-12-22 16:38:54 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,17 @@
+let head = document.getElementById("quote")
+head.innerText = "If you want to shine like a sun, first burn like a sun."
+
+
+
+let para = document.getElementsByClassName("info-box")
+para[0].style.backgroundColor='silver'
+para[1].style.backgroundColor='silver'
+para[2].style.backgroundColor='silver'
+
+
+let lis= document.getElementsByTagName("li")
+lis[2].textContent="Good evening"
+
+
+let foot = document.querySelector("span")
+foot.textContent += ". Have a great day"
